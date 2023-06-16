30c354d080e4de67c8b3dac6fd9a1490ffd6cf6e
NellyNel520 <channelharris520@gmail.com>
2023-06-16 03:50:12 -0400

    created comment model with required associations as well as the migration

---
+++ migrations/20230608031708-create-comment.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      likeCount: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comments');
+  }
+};
---
+++ models/comment.js
@@ -0,0 +1,56 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Comment.belongsTo(models.User,{
+        foreignKey:'userId',
+      })
+      Comment.belongsTo(models.Post,{
+        foreignKey:'postId',
+      })
+    }
+  }
+  Comment.init({
+    userName: {
+      allowNull: false,
+      type:DataTypes.STRING},
+    content: {
+      allowNull: false,
+      type:DataTypes.STRING},
+    userId: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      field: 'userId',
+      onDelete:'CASCADE',
+      references:{
+        model:'users',
+        key:'id'
+      }
+    },
+    postId: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      onDelete:'CASCADE',
+      references:{
+        model:'posts',
+        key:'id'
+      }
+     },
+    likeCount: {
+      allowNull: true,
+      type:DataTypes.INTEGER}
+  }, {
+    sequelize,
+    modelName: 'Comment',
+    tableName: 'comments'
+  });
+  return Comment;
+};
