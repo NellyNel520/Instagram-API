bcee5f28caf2c8a30d816742b7b056afd33b8c7e
NellyNel520 <channelharris520@gmail.com>
2023-06-16 22:10:00 -0400

    created comment controller

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,13 @@
 const Router = require("express").Router();
 const UserRouter = require("./UserRouter");
+const AuthRouter = require("./AuthRouter");
+// const middleware = require("../middleware");
 
 
 
 
 
 Router.use("/users", UserRouter);
+Router.use("/auth", AuthRouter);
+
+module.exports = Router;
